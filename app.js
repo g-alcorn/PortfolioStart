3f442c3eaf7ce0b80fe5da84864991a1b63bae23
Griffin Alcorn <griffin.alcorn@gmail.com>
2021-05-27 12:17:23 -0400

    EJS setup

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const http = require('http');
-const PORT = 8080;
+const express = require("express");
+const app = express();
+const PORT = 3008; // default port 3008
 
-const requestHandler = function(req, res) {
-  res.end(`Request path: ${req.url}\nRequest method: ${req.method}`)
-}
+app.set('view engine', 'ejs');
 
-const server = http.createServer(requestHandler);
+app.get("/", (req, res) => {
+  res.render(index);
+});
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
