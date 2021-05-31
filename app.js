e10e904ccc2cf7fc538a1ae2ef98e00db7fba295
Griffin Alcorn <griffin.alcorn@gmail.com>
2021-05-31 13:52:06 -0400

    navbar improved, contact page started, ejs module installed

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 3008; // default port 3008
 
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.render(index);
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('pages/contact');
 });
 
 app.listen(PORT, () => {
