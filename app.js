350216075f30bc528360b7f0b7cd004527ad5e0a
Griffin Alcorn <griffin.alcorn@gmail.com>
2021-06-03 11:02:55 -0400

    node-sass implemented

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const sass = require('node-sass');
 const app = express();
 const PORT = 3008; // default port 3008
 
+//Set view engine to EJS, use SCSS middleware for styling
 app.set('view engine', 'ejs');
+app.use(sass.middleware({ src: __dirname + '/public' }));
 
 app.get('/', (req, res) => {
   res.render('pages/index');
